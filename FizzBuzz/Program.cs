c561e3407928c3ae939fea99da41f4e92b129c27
davidbeje <david.bejenariu@softwire.com>
2022-07-11 18:22:23 +0300

    almost done part 2

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzBuzz
 {
@@ -8,22 +9,65 @@ namespace FizzBuzz
         {
             for (int i = 1; i <= 100; ++i)
             {
-                if (i % 15 == 0)
+                var ans = new List<String>();
+
+                if (i % 3 == 0)
                 {
-                    Console.WriteLine("FizzBuzz");
-                } 
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                } 
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
+                    ans.Add("Fizz");
                 }
-                else
+
+                if (i % 5 == 0)
                 {
-                    Console.WriteLine(i);
+                    ans.Add("Buzz");
                 }
+
+                if (i % 7 == 0)
+                {
+                    if (ans.Count == 2 || ans.Count == 0)
+                    {
+                        ans.Clear();
+                    }
+
+                    ans.Add("Bang");
+                }
+
+                if (i % 11 == 0)
+                {
+                    ans.Clear();
+                    ans.Add("Bong");
+                }
+
+                if (i % 13 == 0)
+                {
+                    ans.Add("Fezz");
+                    int pos = 0;
+
+                    for (int j = 0; j < ans.Count; ++j)
+                    {
+                        if (ans[j][0] == 'B')
+                        {
+                            pos = j;
+                            break;
+                        }
+                    }
+
+                    if (pos != ans.Count)
+                    {
+                        for (int j = ans.Count - 1; j > pos; --j)
+                        {
+                            ans[j] = ans[j - 1];
+                        }
+
+                        ans[pos] = "Fezz";
+                    }
+                }
+
+                if (i % 17 == 0)
+                {
+                    ans.Reverse();
+                }
+
+                foreach (var)
             }
         }
     }
